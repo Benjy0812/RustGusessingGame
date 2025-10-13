9af7ef2da9165d59cc7abceee12b5cacb90d70b5
Benjy0812 <108727379+Benjy0812@users.noreply.github.com>
2025-10-13 10:09:32 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,72 @@
-#![allow(unused)]
-
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self, Write};
+use colored::*;
 
 fn main() {
-    println!("Guess the number!");
+    println!("{}", "Guess the number!".bright_blue().bold());
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1..11);
+    loop {
+        print!("{}", "Enter difficulty level (1: 1-10, 2: 1-25, 3: 1-50, 4: 1-100): ".yellow());
+        io::stdout().flush().expect("Failed to flush stdout");
 
-    //println!("The secret number is: {}", secret_number);
+        let mut difficulty = String::new();
+        io::stdin()
+            .read_line(&mut difficulty)
+            .expect("Failed to read line");
 
-    println!("Please input your guess.");
+        let max_number = match difficulty.trim() {
+            "1" => 10,
+            "2" => 25,
+            "3" => 50,
+            "4" => 100,
+            _ => {
+                println!("{}", "Invalid input, defaulting to level 1 (1-10).".red());
+                10
+            }
+        };
 
-    let mut guess: String = String::new();
+        let mut rng = rand::rng();
+        let secret_number = rng.random_range(1..=max_number);
 
-    io::stdin().read_line(&mut guess).expect("Please type a number!");
+        loop {
+            print!("{}", "Please input your guess: ".yellow());
+            io::stdout().flush().expect("Failed to flush stdout");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+            let mut guess = String::new();
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("{}", "Please enter a valid number!".red());
+                    continue;
+                }
+            };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+            println!("You guessed: {}", guess.to_string().cyan());
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("{}", "That's too small!".magenta()),
+                Ordering::Greater => println!("{}", "That's too big!".magenta()),
+                Ordering::Equal => {
+                    println!("{}", "You win!".bright_green().bold());
+                    break;
+                }
+            }
+        }
+
+        print!("{}", "Play again? (y/n): ".yellow());
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let mut again = String::new();
+        io::stdin()
+            .read_line(&mut again)
+            .expect("Failed to read line");
+        if again.trim().to_lowercase() != "y" {
+            break;
+        }
     }
 }
